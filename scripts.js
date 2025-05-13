767594a2b788f75a03aa4639a2f5c60bf92e2e75
naussen <nvpezat@gmail.com>
2025-05-13 19:39:32 -0300

    novo

--- scripts.js
+++ scripts.js
@@ -19,7 +19,6 @@ try {
     alert("Falha grave ao inicializar o sistema. Verifique o console.");
 }
 
-document.addEventListener('DOMContentLoaded', () => {
     // Tema Claro/Escuro (comum a todas as páginas)
     const themeToggleBtn = document.getElementById('theme-toggle');
     const bodyElement = document.body;
